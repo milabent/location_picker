2c72b288e7a1e288736e597e7b66a36ee7d56644
Sebastian Gaul <sebastian@mgvmedia.com>
2015-05-20 15:40:36 +0200

    Fix Formtastic Input issue

--- app/inputs/location_input.rb
+++ app/inputs/location_input.rb
@@ -1,47 +1,49 @@
-class LocationInput
-  include Formtastic::Inputs::Base
+if defined?(::Formtastic)
+  class LocationInput
+    include Formtastic::Inputs::Base
 
-  def to_html
-    input_wrapping do
-      label_html << input_fields
+    def to_html
+      input_wrapping do
+        label_html << input_fields
+      end
     end
-  end
 
-  def input_fields
-    template.content_tag(:div, class: 'location-picker', data: { picker: 'location', field_prefix: @object_name }) do
-      loading_indicator <<
-      address_error_indicator <<
-      location_field <<
-      action_buttons <<
-      builder.hidden_field(:longitude) <<
-      builder.hidden_field(:latitude)
+    def input_fields
+      template.content_tag(:div, class: 'location-picker', data: { picker: 'location', field_prefix: @object_name }) do
+        loading_indicator <<
+        address_error_indicator <<
+        location_field <<
+        action_buttons <<
+        builder.hidden_field(:longitude) <<
+        builder.hidden_field(:latitude)
+      end
     end
-  end
 
-  def location_field
-    template.content_tag(:div, '', class: 'location-picker-map')
-  end
-
-  def loading_indicator
-    template.content_tag(:div, class: 'location-picker-loading') do
-      I18n.translate('location_picker.loading')
+    def location_field
+      template.content_tag(:div, '', class: 'location-picker-map')
     end
-  end
 
-  def address_error_indicator
-    template.content_tag(:div, class: 'location-picker-address-error') do
-      I18n.translate('location_picker.address_not_found')
+    def loading_indicator
+      template.content_tag(:div, class: 'location-picker-loading') do
+        I18n.translate('location_picker.loading')
+      end
     end
-  end
 
-  def action_buttons
-    template.content_tag(:div, class: 'location-picker-buttons') do
-      action_button(:reset) << action_button(:search)
+    def address_error_indicator
+      template.content_tag(:div, class: 'location-picker-address-error') do
+        I18n.translate('location_picker.address_not_found')
+      end
     end
-  end
 
-  def action_button(type)
-    label = I18n.translate("location_picker.#{type}")
-    template.content_tag(:button, label, class: "location-picker-#{type}-button", type: :button)
+    def action_buttons
+      template.content_tag(:div, class: 'location-picker-buttons') do
+        action_button(:reset) << action_button(:search)
+      end
+    end
+
+    def action_button(type)
+      label = I18n.translate("location_picker.#{type}")
+      template.content_tag(:button, label, class: "location-picker-#{type}-button", type: :button)
+    end
   end
 end
